e419c26193d2612cdf85859bf8d9a97a63dab38f
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-07-12 14:15:22 +0200

    add custom colors

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text className="bg-red-500">
+      <Text className="bg-accent">
         Open up App.tsx to start working on your app!
       </Text>
       <StatusBar style="auto" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-// tailwind.config.js
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      primary: colors.stone[200],
+      secondary: colors.slate[900],
+      accent: colors.indigo[400],
+    },
     extend: {},
   },
   plugins: [],
