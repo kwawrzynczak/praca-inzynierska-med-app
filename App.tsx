be30545ffc30f9aa8ca63232d84984c4a1b87780
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-07-12 14:08:59 +0200

    add nativewind

--- App.tsx
+++ App.tsx
@@ -3,20 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Text className="bg-red-500">
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 export default App;
