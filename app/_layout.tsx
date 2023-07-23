7c82fcdeb133233610cdad20fbad8f83036582b1
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-07-23 12:33:37 +0200

    finish StepAccountType component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,6 +32,7 @@ const StackLayout = () => {
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="register" options={{ headerShown: true, title: 'Rejestracja' }} />
     </Stack>
   );
 };
--- src/components/StepAccountType/StepAccountType.tsx
+++ src/components/StepAccountType/StepAccountType.tsx
@@ -1,12 +1,17 @@
 import { Pressable, View } from 'react-native';
 import { Text } from '@components';
+import { CaretakerImage, UserImage } from '@images';
 
 export const StepAccountType = () => {
   return (
     <View className="flex-1 items-center justify-center">
-      <Pressable className="h-52 w-52 rounded-full border bg-accent active:bg-blue-900" />
+      <Pressable className="h-52 w-52 justify-center rounded-full bg-accent/60 active:bg-blue-600/60">
+        <UserImage width="190px" height="190px" />
+      </Pressable>
       <Text className="mt-4 text-center font-medium text-lg text-secondary">Jestem podopiecznym</Text>
-      <Pressable className="mt-10 h-52 w-52 items-center justify-center rounded-full border bg-accent active:bg-blue-900" />
+      <Pressable className="mt-10 h-52 w-52 rounded-full bg-accent/60 active:bg-blue-600/60">
+        <CaretakerImage width="208px" height="208px" />
+      </Pressable>
       <Text className="mt-4 text-center font-medium text-lg text-secondary">Jestem opiekunem</Text>
     </View>
   );
