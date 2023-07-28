0d7aad856d8a305d4d69fb045945f0bbff4f1d73
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-07-28 13:42:14 +0200

    update StepAccountType logic

--- src/components/StepAccountType/StepAccountType.tsx
+++ src/components/StepAccountType/StepAccountType.tsx
@@ -5,29 +5,25 @@ import { CaretakerImage, PatientImage } from '@images';
 import clsx from 'clsx';
 
 export const StepAccountType = () => {
-  const [active1, setActive1] = useState(false);
-  const [active2, setActive2] = useState(false);
-  const onPatientPress = () => {
-    setActive1(true);
-    if (active2) setActive2(false);
-  };
-  const onCaretakerPress = () => {
-    setActive2(true);
-    if (active1) setActive1(false);
-  };
-
+  const [selectedId, setSelectedId] = useState<string>();
   return (
     <View className="h-3/4 items-center justify-center">
       <Pressable
-        className={clsx('h-52 w-52 justify-center rounded-full bg-accent/60', active1 && 'scale-105 bg-blue-600/60')}
-        onPress={onPatientPress}
+        className={clsx(
+          'h-52 w-52 justify-center rounded-full bg-accent/60',
+          selectedId === 'patient' && 'scale-105 bg-blue-600/60',
+        )}
+        onPress={() => setSelectedId('patient')}
       >
         <PatientImage width="190px" height="190px" />
       </Pressable>
       <Text className="mb-6 mt-4 text-center font-medium text-lg text-secondary">Jestem podopiecznym</Text>
       <Pressable
-        className={clsx('h-52 w-52 justify-center rounded-full bg-accent/60', active2 && 'scale-105 bg-blue-600/60')}
-        onPress={onCaretakerPress}
+        className={clsx(
+          'h-52 w-52 justify-center rounded-full bg-accent/60',
+          selectedId === 'caretaker' && 'scale-105 bg-blue-600/60',
+        )}
+        onPress={() => setSelectedId('caretaker')}
       >
         <CaretakerImage width="208px" height="208px" />
       </Pressable>
