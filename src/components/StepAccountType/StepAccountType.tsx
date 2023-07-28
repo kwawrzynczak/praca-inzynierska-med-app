cdd672bfdf7ce4490a94b8e61e3a121796f16c6f
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-07-28 14:05:49 +0200

    map buttons in StepAccountType

--- src/components/StepAccountType/StepAccountType.tsx
+++ src/components/StepAccountType/StepAccountType.tsx
@@ -4,30 +4,37 @@ import { Text } from '@components/Text';
 import { CaretakerImage, PatientImage } from '@images';
 import clsx from 'clsx';
 
+const ROLE_BUTTONS = [
+  {
+    id: 'patient',
+    text: 'Jestem podopiecznym',
+    image: <PatientImage width="190px" height="190px" />,
+  },
+  {
+    id: 'caretaker',
+    text: 'Jestem opiekunem',
+    image: <CaretakerImage width="190px" height="190px" />,
+  },
+];
+
 export const StepAccountType = () => {
   const [selectedId, setSelectedId] = useState<string>();
   return (
     <View className="h-3/4 items-center justify-center">
-      <Pressable
-        className={clsx(
-          'h-52 w-52 justify-center rounded-full bg-accent/60',
-          selectedId === 'patient' && 'scale-105 bg-blue-600/60',
-        )}
-        onPress={() => setSelectedId('patient')}
-      >
-        <PatientImage width="190px" height="190px" />
-      </Pressable>
-      <Text className="mb-6 mt-4 text-center font-medium text-lg text-secondary">Jestem podopiecznym</Text>
-      <Pressable
-        className={clsx(
-          'h-52 w-52 justify-center rounded-full bg-accent/60',
-          selectedId === 'caretaker' && 'scale-105 bg-blue-600/60',
-        )}
-        onPress={() => setSelectedId('caretaker')}
-      >
-        <CaretakerImage width="208px" height="208px" />
-      </Pressable>
-      <Text className="mt-4 text-center font-medium text-lg text-secondary">Jestem opiekunem</Text>
+      {ROLE_BUTTONS.map(({ id, text, image }) => (
+        <>
+          <Pressable
+            className={clsx(
+              'h-52 w-52 justify-center rounded-full bg-accent/60',
+              selectedId === id && 'scale-105 bg-blue-600/60',
+            )}
+            onPress={() => setSelectedId(id)}
+          >
+            {image}
+          </Pressable>
+          <Text className="mb-6 mt-4 text-center font-medium text-lg text-secondary">{text}</Text>
+        </>
+      ))}
     </View>
   );
 };
