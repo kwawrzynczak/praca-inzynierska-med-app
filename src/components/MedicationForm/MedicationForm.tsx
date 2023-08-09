202cf10dba1f55ce4f93206f3ed2fc9bf4e47f31
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-08-09 10:13:44 +0200

    create MedicationForm component

---
+++ src/components/MedicationForm/MedicationForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const MedicationForm = () => {
+  return (
+    <View className="items-center justify-center">
+      <Text>MedicationForm</Text>
+    </View>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './Button';
 export * from './FAB';
 export * from './Input';
+export * from './MedicationForm';
 export * from './MedicationListElement';
 export * from './StepAccountType';
 export * from './StepRegisterForm';
