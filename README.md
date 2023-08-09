41b7e2c2db9e64fc9668c36ee404abeb766f13c0
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-08-09 11:51:30 +0200

    update todo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## TODO:
 
 - resetowanie stanu BottomSheet po opuszczeniu ekranu Medication
+- dodanie Taba/Ustawień z listą podopiecznych lub opiekunów
+- resertowanie hasła
+- wysyłanie zaproszenia do podopiecznego
+- powiadomienia
--- src/components/StepRegisterForm/StepRegisterForm.tsx
+++ src/components/StepRegisterForm/StepRegisterForm.tsx
@@ -1,55 +1,35 @@
-import { Pressable, View } from 'react-native';
-import { Button } from '@components/Button';
+import { View } from 'react-native';
 import { Input } from '@components/Input';
-import { Text } from '@components/Text';
-import { Link } from 'expo-router';
-import { Formik } from 'formik';
 
 export const StepRegisterForm = () => {
   return (
     <View>
-      <Formik
-        initialValues={{ email: '', password: '', validatePassword: '' }}
-        onSubmit={(values) => console.log(values)}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
-          <View>
-            <View>
-              <Input
-                title="Adres e-mail"
-                placeholder="uzytkownik@example.com"
-                placeholderTextColor="#666"
-                textContentType="emailAddress"
-                onChangeText={handleChange('email')}
-                onBlur={handleBlur('email')}
-                keyboardType="email-address"
-                value={values.email}
-              />
+      <View>
+        <View>
+          <Input
+            title="Adres e-mail"
+            placeholder="uzytkownik@example.com"
+            placeholderTextColor="#666"
+            textContentType="emailAddress"
+            keyboardType="email-address"
+          />
 
-              <Input
-                title="Hasło"
-                placeholder="**********"
-                placeholderTextColor="#666"
-                textContentType="password"
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
-                value={values.password}
-                secureTextEntry
-              />
-              <Input
-                title="Potwierdź hasło"
-                placeholder="**********"
-                placeholderTextColor="#666"
-                textContentType="password"
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
-                value={values.validatePassword}
-                secureTextEntry
-              />
-            </View>
-          </View>
-        )}
-      </Formik>
+          <Input
+            title="Hasło"
+            placeholder="**********"
+            placeholderTextColor="#666"
+            textContentType="password"
+            secureTextEntry
+          />
+          <Input
+            title="Potwierdź hasło"
+            placeholder="**********"
+            placeholderTextColor="#666"
+            textContentType="password"
+            secureTextEntry
+          />
+        </View>
+      </View>
     </View>
   );
 };
