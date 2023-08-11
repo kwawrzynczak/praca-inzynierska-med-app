63d787ed623b46f3da6343775fdb994fdf0abff4
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-08-11 15:53:15 +0200

    create custom TabBar component

--- app/(tabs)/calendar/_layout.tsx
+++ app/(tabs)/calendar/_layout.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/no-unstable-nested-components */
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator, MaterialTopTabBarProps } from '@react-navigation/material-top-tabs';
 
 import ActiveScreen from './active';
 import InactiveScreen from './inactive';
@@ -9,26 +9,7 @@ const Tab = createMaterialTopTabNavigator();
 
 const CalendarLayout = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        swipeEnabled: false,
-        lazy: true,
-        // tabBarActiveTintColor: '#AFAFAF',
-        // tabBarInactiveTintColor: '#E4E4',
-        // tabBarIndicatorStyle: {
-        //   backgroundColor: '#E4',
-        // },
-        // tabBarIndicatorContainerStyle: {
-        //   backgroundColor: 'blue',
-        // },
-        // tabBarContentContainerStyle: {
-        //   backgroundColor: 'green',
-        // },
-        tabBarStyle: {
-          backgroundColor: 'pink',
-        },
-      }}
-    >
+    <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
       <Tab.Screen name="Active" component={ActiveScreen} options={{ tabBarLabel: 'Aktywne' }} />
       <Tab.Screen name="Inactive" component={InactiveScreen} options={{ tabBarLabel: 'Zakończone' }} />
     </Tab.Navigator>
