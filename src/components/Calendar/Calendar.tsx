5b28f94bb9fee9aeac5393a9f13eb90907ff2ff0
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-09-03 21:05:06 +0200

    add CalendarStripe component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,35 +1,19 @@
 import { FlatList, View } from 'react-native';
+import CalendarStrip from 'react-native-calendar-strip';
 import { Text } from '@components/Text';
+import moment from 'moment';
 
-const DATA = [
-  { name: 'pon', day: 1 },
-  { name: 'wt', day: 2 },
-  { name: 'śr', day: 3 },
-  { name: 'czw', day: 4 },
-  { name: 'pt', day: 5 },
-  { name: 'sob', day: 6 },
-];
+import 'moment/locale/pl';
+
 export const Calendar = () => {
   return (
-    <View className="container items-center">
-      <Text variant="subtitle">styczeń 2024</Text>
-      <FlatList
-        className="h-24 pt-2"
-        contentContainerStyle={{ alignItems: 'center' }}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        data={DATA}
-        renderItem={({ item }) => (
-          <View className="mx-3 h-16 w-12 items-center justify-center rounded-lg bg-accent">
-            <Text className="font-bold text-white">{item.name}</Text>
-            <Text className="font-semibold text-xl text-white">{item.day}</Text>
-          </View>
-        )}
-        keyExtractor={(item) => item.name}
+    <View className="container">
+      <CalendarStrip
+        scrollable
+        calendarHeaderStyle={{ color: '#666', fontFamily: 'NunitoSans_700Bold' }}
+        style={{ height: 80 }}
       />
-      <Text variant="subtitle" className="my-2">
-        poniedziałek, 1 stycznia
-      </Text>
+      <Text className="my-2 text-center font-bold">{moment().format('LL')}</Text>
     </View>
   );
 };
