3a5be6ca97d2fcba1b001385941cd29b3523f4f1
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-10-06 00:49:55 +0200

    change tab style depending on focus

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
+const activeLabel = '#111111';
+const inctiveLabel = '#222222';
 const TabsLayout = () => (
   <Tabs>
     <Tabs.Screen
       name="medication"
       options={{
         tabBarLabel: 'Leki',
+        tabBarLabelStyle: { fontSize: 12, fontWeight: '500' },
         headerShown: false,
-        tabBarIcon: ({ size }) => <FontAwesome5 name="pills" size={size} color="#809fff" />,
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        tabBarIcon: ({ size, focused }) => (
+          <FontAwesome5 name="pills" size={size} color={focused ? '#111' : '#809fff'} />
+        ),
+        tabBarActiveTintColor: activeLabel,
+        tabBarInactiveTintColor: inctiveLabel,
         unmountOnBlur: false,
       }}
     />
@@ -19,15 +28,24 @@ const TabsLayout = () => (
       options={{
         headerShown: false,
         tabBarLabel: 'Wizyty',
-        tabBarIcon: ({ size }) => <FontAwesome5 name="stethoscope" size={size} color="#809fff" />,
+        tabBarLabelStyle: { fontSize: 12, fontWeight: '500' },
+        tabBarIcon: ({ size, focused }) => (
+          <FontAwesome5 name="stethoscope" size={size} color={focused ? '#111' : '#809fff'} />
+        ),
+
+        tabBarActiveTintColor: activeLabel,
+        tabBarInactiveTintColor: inctiveLabel,
       }}
     />
     <Tabs.Screen
       name="settings"
       options={{
         tabBarLabel: 'Ustawienia',
+        tabBarLabelStyle: { fontSize: 12, fontWeight: '500' },
         headerTitle: 'Ustawienia',
-        tabBarIcon: ({ size }) => <FontAwesome5 name="cog" size={size} color="#809fff" />,
+        tabBarIcon: ({ size, focused }) => <FontAwesome5 name="cog" size={size} color={focused ? '#111' : '#809fff'} />,
+        tabBarActiveTintColor: activeLabel,
+        tabBarInactiveTintColor: inctiveLabel,
       }}
     />
   </Tabs>
